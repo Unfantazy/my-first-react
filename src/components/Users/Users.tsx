fd5777d5a53262cc1d093e6a7cc5481305d0c650
Angelina <unliantazy@gmail.com>
2021-04-13 20:14:01 +0300

    refactor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,60 +1,64 @@
 import React from 'react'
 import s from '../../styles/users.module.css';
-import axios from "axios";
 import userPhoto from '../../assets/images/avatar.svg'
 
-export const Users = (props: any) => {
+export let Users = (props: any) => {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios
-                .get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    props.setUsers(response.data.items)
-                })
-        }
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
     return <div className={s.users}>
 
-        <button onClick={getUsers}>Get Users</button>
-        <br/>
-        <br/>
+        <div className={s.pagination}>
+            {pages.slice(0, 10).map(p => {
+                return <span className={props.currentPage === p ? s.selectedPage : s.paginationPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}> {p} </span>
+            })}
+        </div>
 
-        {props.users.map((u: any) =>
-            <div key={u.id} className={s.userBlock}>
+        {
+            props.users.map((u: any) =>
+                <div key={u.id} className={s.userBlock}>
 
-                <div className={s.userShow}>
-                    <div className={s.userAvatar}>
-                        <img
-                            src={u.photos.small !== null ? u.photos.small : userPhoto}
-                            alt="avatar"/>
+                    <div className={s.userShow}>
+                        <div className={s.userAvatar}>
+                            <img
+                                src={u.photos.small !== null ? u.photos.small : userPhoto}
+                                alt="avatar"/>
+                        </div>
+                        <div className={s.userFollowBtnWrapper}>
+
+                            {u.followed ?
+                                <button className={s.userFollowBtn} type={"button"} onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Unfollow</button> :
+                                <button className={s.userFollowBtn} type={"button"} onClick={() => {
+                                    props.follow(u.id)
+                                }}>Follow</button>}
+                        </div>
+
                     </div>
-                    <div className={s.userFollowBtnWrapper}>
 
-                        {u.followed ?
-                            <button className={s.userFollowBtn} type={"button"} onClick={() => {
-                                props.unfollow(u.id)
-                            }}>Follow</button> :
-                            <button className={s.userFollowBtn} type={"button"} onClick={() => {
-                                props.follow(u.id)
-                            }}>Unfollow</button>}
+                    <div className={s.userInfo}>
+                        <div className={s.userInfoLeft}>
+                            <div className={s.userInfoName}>{u.name}</div>
+                            <div className={s.userInfoStatus}>{u.status}</div>
+                        </div>
+                        <div className={s.userInfoLocation}>
+                            <span className={s.userInfoCountry}>{"u.location.country"},</span>
+                            <span className={s.userInfoCity}>{"u.location.city"}</span>
+                        </div>
                     </div>
-
                 </div>
-
-                <div className={s.userInfo}>
-                    <div className={s.userInfoLeft}>
-                        <div className={s.userInfoName}>{u.name}</div>
-                        <div className={s.userInfoStatus}>{u.status}</div>
-                    </div>
-                    <div className={s.userInfoLocation}>
-                        <span className={s.userInfoCountry}>{"u.location.country"},</span>
-                        <span className={s.userInfoCity}>{"u.location.city"}</span>
-                    </div>
-                </div>
-            </div>
-        )}
+            )
+        }
     </div>
-
 }
+
+export default Users;
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import {connect} from "react-redux";
-import Users from "./UsersClass";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import axios from "axios";
+import UsersFunc from "./Users";
 
-// export const UsersContainer = () => {
-//     return <div>
-//         USERS WILL BE HERE
-//     </div>
-//
-// }
+class UsersAPI extends React.Component<any, any> {
+
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber: any) => {
+        this.props.setCurrentPage(pageNumber)
+
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <UsersFunc totalUsersCount={this.props.totalUsersCount}
+                          pageSize={this.props.pageSize}
+                          currentPage={this.props.currentPage}
+                          onPageChanged={this.onPageChanged}
+                          users={this.props.users}
+                          follow={this.props.follow}
+                          unfollow={this.props.unfollow}
+        />
+    }
+}
+
 
 let mapStateToProps = (state: any) => {
     return {
@@ -39,4 +67,4 @@ let mapDispatchToProps = (dispatch: any) => {
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPI)
